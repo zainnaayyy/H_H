60ad29aaa4018bfb8c67af4b31dde2b7937be05b
talha kazmi <talhakazmi694@gmail.com>
2024-12-13 16:59:46 +0500

    fix links

--- components/GetQuoteRibbon.jsx
+++ components/GetQuoteRibbon.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const GetQuoteRibbon = () => {
   return (
     <div className="fixed right-0 top-1/2 transform -translate-y-1/2 z-50">
-      <a href="/quote">
+      <a href="/appointment">
         <Image src="/images/quoteOriginal.png" width={150} height={200} alt="Get a Quote" />
       </a>
     </div>
